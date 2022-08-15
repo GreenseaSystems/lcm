9a8bf68f27d75724d8e4330bcd2a5106a3fd7c8b
Chris Burnham <cburnham@greensea.com>
2022-08-15 12:00:42 -0400

    adding build script

---
+++ build_lcm_1.3.1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# run with sudo
+dir=$(pwd)
+version=1.3.1
+wget -O lcm-${version}.tar.gz https://github.com/lcm-proj/lcm/archive/v${version}.tar.gz
+tar xvf lcm-${version}.tar.gz
+cd lcm-${version}
+./bootstrap.sh
+./configure
+make -j4
+checkinstall --nodoc \
+  --fstrans=no \
+  --pkgname=lcm \
+  --pkggroup=GSS \
+  --pkgversion=${version} \
+  --provides=lcm \
+  --backup=no \
+  -y \
+  --install=no \
+  make install
+mv *.deb $dir
